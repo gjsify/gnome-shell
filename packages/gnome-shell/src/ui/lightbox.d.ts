fed605e7f261aa0c54b908338879eb3304cac72e
Totto16 <tobiausgais@gmail.com>
2024-03-28 16:44:48 +0100

    add version specifier to checked classes in ui/lightbox and add protected methods too

--- packages/gnome-shell/src/ui/lightbox.d.ts
+++ packages/gnome-shell/src/ui/lightbox.d.ts
@@ -14,6 +14,9 @@ export interface RadialShaderEffectProps
     sharpness?: number
 }
 
+/**
+ * @version 46
+ */
 export class RadialShaderEffect extends Shell.GLSLEffect {
     protected _brightness: number
     protected _sharpness: number
@@ -42,17 +45,47 @@ export interface LightboxProps
     sharpness?: number
 }
 
+/**
+ * @version 46
+ */
 export class Lightbox extends St.Bin {
     protected _active: boolean
 
     public readonly active: boolean
 
+    /**
+     * Lightbox creates a dark translucent "shade" actor to hide the
+     * contents of `container`, and allows you to specify particular actors
+     * in `container` to highlight by bringing them above the shade. It
+     * tracks added and removed actors in `container` while the lightboxing
+     * is active, and ensures that all actors are returned to their
+     * original stacking order when the lightboxing is removed. (However,
+     * if actors are restacked by outside code while the lightboxing is
+     * active, the lightbox may later revert them back to their original
+     * order.)
+     *
+     * By default, the shade window will have the height and width of
+     * `container` and will track any changes in its size. You can override
+     * this by passing an explicit width and height in `params`.
+     *
+     * @param {Clutter.Container} container parent Clutter.Container
+     * @param {object} [params] additional parameters:
+     * @param {boolean=} params.inhibitEvents: whether to inhibit events for `container`
+     * @param {number=} params.width: shade actor width
+     * @param {number=} params.height: shade actor height
+     * @param {number=} params.fadeFactor: fading opacity factor
+     * @param {boolean=} params.radialEffect: whether to enable the GLSL radial effect
+     */
     constructor(container: Clutter.Actor, params?: LightboxProps)
     public _init(container: Clutter.Actor, params?: LightboxProps): void
 
     lightOn(fadeInTime?: number): void
-
     lightOff(fadeOutTime?: number): void
 
     highlight(window: Clutter.Actor): void
+
+    protected _childAdded(container: Clutter.Actor, newChild: Clutter.Actor): void;
+    protected _childRemoved(container: Clutter.Actor, child: Clutter.Actor): void;
+    protected _onDestroy(): void;
+
 }
