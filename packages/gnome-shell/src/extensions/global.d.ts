bd492b213cf23e7bfab8ab119239b30f2dcd85e6
Totto16 <tobiausgais@gmail.com>
2024-04-17 09:14:59 +0200

    ad version annotations to the newly added types

--- packages/gnome-shell/src/extensions/global.d.ts
+++ packages/gnome-shell/src/extensions/global.d.ts
@@ -29,11 +29,16 @@ declare global {
          * @param x The value to be clamped.
          * @param min The lower bound of the result.
          * @param max The upper bound of the result.
+         *
+         * @version 46
          */
         clamp(x: number, min: number, max: number): number;
     }
 }
 
+/**
+ * @version 46
+ */
 interface EasingParams {
     // milliseconds
     duration?: number;
@@ -50,6 +55,7 @@ interface EasingParams {
  *
  * @see https://gitlab.gnome.org/GNOME/gnome-shell/-/blob/54bc3aa4f54cb5452c29f81fada808224a18afa1/js/ui/environment.js#L133
  *
+ * @version 46
  * Note: this list is non exhaustive, since its never typed anywhere else, each parameter is just string in e.g remove_transition, where this is used, so these here are verified manually, but there might be more
  */
 type AnimatableActorFields =
@@ -88,6 +94,7 @@ declare module '@girs/st-14' {
             /**
              * A convenience wrapper for adjustments
              *
+             * @version 46
              * @see https://gitlab.gnome.org/GNOME/gnome-shell/-/blob/96e27f0e7d4e0c71976305d0d2c36a6c39d9853c/docs/js-coding-style.md#animations
              * @see https://gitlab.gnome.org/GNOME/gnome-shell/-/blob/54bc3aa4f54cb5452c29f81fada808224a18afa1/js/ui/environment.js#L292
              */
@@ -102,6 +109,7 @@ declare module '@girs/clutter-14' {
             /**
              * A convenience wrapper for actors
              *
+             * @version 46
              * @see https://gitlab.gnome.org/GNOME/gnome-shell/-/blob/96e27f0e7d4e0c71976305d0d2c36a6c39d9853c/docs/js-coding-style.md#animations
              * @see https://gitlab.gnome.org/GNOME/gnome-shell/-/blob/54bc3aa4f54cb5452c29f81fada808224a18afa1/js/ui/environment.js#L286
              */
