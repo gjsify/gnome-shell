12c2286307354ffcdb623d9464267cb239710069
Totto16 <tobiausgais@gmail.com>
2024-04-17 21:42:07 +0200

    recheck types, add version and links to extensionSystem and extension metadata

--- packages/gnome-shell/src/types/extension-metadata.ts
+++ packages/gnome-shell/src/types/extension-metadata.ts
@@ -1,5 +1,12 @@
+// https://gitlab.gnome.org/GNOME/gnome-shell/-/blob/main/js/extensions/sharedInternals.js
+
 import type Gio from '@girs/gio-2.0';
 
+/** The Metadata Object for Extensions
+ *
+ * @see https://gitlab.gnome.org/GNOME/gnome-shell/-/blob/main/js/extensions/sharedInternals.js#L48
+ * @version 46
+ */
 export interface ExtensionMetadata {
     readonly uuid: string;
     readonly dir: Gio.File;
--- packages/gnome-shell/src/ui/extensionSystem.d.ts
+++ packages/gnome-shell/src/ui/extensionSystem.d.ts
@@ -6,8 +6,14 @@ import * as Signals from '../misc/signals.js';
 import type { ExtensionState, ExtensionType } from '../misc/extensionUtils.js';
 import type { Extension } from '../extensions/extension.js';
 
-// This is different from the one in extension.js because it encodes the raw
-// metadata loaded directly from metadata.json
+/** The Metadata Object for Extensions
+ *
+ * This is different from the one in extension.js because it encodes the raw
+ * metadata loaded directly from metadata.json
+ *
+ * @see https://gitlab.gnome.org/GNOME/gnome-shell/-/blob/main/js/ui/extensionSystem.js#L400
+ * @version 46
+ */
 interface ExtensionMetadata extends Record<string, any> {
     // GNOME Shell checks these properties
     readonly uuid: string;
@@ -16,6 +22,12 @@ interface ExtensionMetadata extends Record<string, any> {
     readonly 'shell-version': readonly string[];
 }
 
+/**
+ *
+ *
+ * @see https://gitlab.gnome.org/GNOME/gnome-shell/-/blob/main/js/misc/extensionUtils.js#L30
+ * @version 46
+ */
 export interface ExtensionObject {
     readonly metadata: ExtensionMetadata;
     readonly uuid: string;
@@ -31,18 +43,27 @@ export interface ExtensionObject {
     readonly stateObj?: Extension;
 }
 
+/**
+ * @see https://gitlab.gnome.org/GNOME/gnome-shell/-/blob/main/js/dbusServices/extensions/extensionsService.js#L15
+ * @version 46
+ */
 export namespace ExtensionManager {
     interface SignalMap {
         readonly 'extension-state-changed': [extension: ExtensionObject];
     }
 
-    // See "OpenExtensionPrefs" in https://gitlab.gnome.org/GNOME/gnome-shell/-/blob/main/data/dbus-interfaces/org.gnome.Shell.Extensions.xml
+    /**
+     *
+     * @see https://gitlab.gnome.org/GNOME/gnome-shell/-/blob/main/data/dbus-interfaces/org.gnome.Shell.Extensions.xml#L234
+     * @version 46
+     */
     interface OpenExtensionPrefsOptions {
         modal?: boolean;
     }
 }
 
 /**
+ * @see https://gitlab.gnome.org/GNOME/gnome-shell/-/blob/main/js/ui/extensionSystem.js#L29
  * @version 46
  */
 export class ExtensionManager<S extends Signals.SignalMap<S> = ExtensionManager.SignalMap> extends Signals.EventEmitter<S> {
