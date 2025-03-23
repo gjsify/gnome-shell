1752816b7c501938653fac191f0d75767073ccaa
Totto16 <tobiausgais@gmail.com>
2025-03-23 16:23:18 +0100

    feat: add example usage of the getLogger() API to the example extension

--- examples/hello-world/src/extension.ts
+++ examples/hello-world/src/extension.ts
@@ -2,12 +2,13 @@ import '@girs/gjs'; // For global types like `log()`
 import St from '@girs/st-16';
 
 import '@girs/gnome-shell/extensions/global'; // For global shell types
-import { Extension, gettext as _ } from '@girs/gnome-shell/extensions/extension';
+import { Extension, gettext as _, type ConsoleLike } from '@girs/gnome-shell/extensions/extension';
 import PanelMenu from '@girs/gnome-shell/ui/panelMenu';
 import * as Main from '@girs/gnome-shell/ui/main';
 
 export default class ExampleExtension extends Extension {
     private _indicator: PanelMenu.Button | null = null;
+    private _console: ConsoleLike | null = null;
 
     override enable() {
         if (this._indicator !== null) {
@@ -26,10 +27,14 @@ export default class ExampleExtension extends Extension {
 
         // Add the indicator to the panel
         Main.panel.addToStatusArea(this.uuid, this._indicator);
+
+        this._console = this.getLogger();
+        this._console?.log('Hello from the example extension');
     }
 
     override disable() {
         this._indicator?.destroy();
         this._indicator = null;
+        this._console = null;
     }
 }
