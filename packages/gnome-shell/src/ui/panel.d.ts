0dc2298289eca04f40eee2b562a488187cf52b81
Zacharie Dubrulle <dubrullezacharie@gmail.com>
2025-07-18 15:55:06 +0200

    feat(ui/panel): add QuickSettings

--- packages/gnome-shell/src/ui/panel.d.ts
+++ packages/gnome-shell/src/ui/panel.d.ts
@@ -7,6 +7,63 @@ import type Meta from '@girs/meta-16';
 import type { Button } from './panelMenu.js';
 import type { DateMenuButton } from './dateMenu.js';
 import { PopupMenuManager } from './popupMenu.js';
+import type { QuickSettingsMenu, SystemIndicator } from './quickSettings.js';
+
+import type * as AutoRotateStatus from './status/autoRotate.js';
+import type * as BackgroundAppsStatus from './status/backgroundApps.js';
+import type * as BacklightStatus from './status/backlight.js';
+import type * as BluetoothStatus from './status/bluetooth.js';
+import type * as BrightnessStatus from './status/brightness.js';
+import type * as CameraStatus from './status/camera.js';
+import type * as DarkModeStatus from './status/darkMode.js';
+import type * as LocationStatus from './status/location.js';
+import type * as NetworkStatus from './status/network.js';
+import type * as NightLightStatus from './status/nightLight.js';
+import type * as PowerProfileStatus from './status/powerProfiles.js';
+import type * as RemoteAccessStatus from './status/remoteAccess.js';
+import type * as RFKillStatus from './status/rfkill.js';
+import type * as SystemStatus from './status/system.js';
+import type * as ThunderboltStatus from './status/thunderbolt.js';
+import type * as VolumeStatus from './status/volume.js';
+
+/**
+ * @version 48
+ */
+export class UnsafeModeIndicator extends SystemIndicator {
+    _indicator: St.Icon;
+}
+
+/**
+ * @version 48
+ */
+export class QuickSettings extends Button {
+    override menu: QuickSettingsMenu;
+    _indicators: St.BoxLayout;
+
+    _network?: NetworkStatus.Indicator | null;
+    _bluetooth?: BluetoothStatus.Indicator | null;
+    _system?: SystemStatus.Indicator;
+    _camera?: CameraStatus.Indicator;
+    _volumeOutput?: VolumeStatus.OutputIndicator;
+    _volumeInput?: VolumeStatus.InputIndicator;
+    _brightness?: BrightnessStatus.Indicator;
+    _remoteAccess?: RemoteAccessStatus.RemoteAccessApplet;
+    _location?: LocationStatus.Indicator;
+    _thunderbolt?: ThunderboltStatus.Indicator;
+    _nightLight?: NightLightStatus.Indicator;
+    _darkMode?: DarkModeStatus.Indicator;
+    _backlight?: BacklightStatus.Indicator;
+    _powerProfiles?: PowerProfileStatus.Indicator;
+    _rfkill?: RFKillStatus.Indicator;
+    _autoRotate?: AutoRotateStatus.Indicator;
+    _unsafeMode?: UnsafeModeIndicator;
+    _backgroundApps?: BackgroundAppsStatus.Indicator;
+
+    addExternalIndicator(indicator: Button, colSpan?: number): void;
+
+    _setupIndicators(): Promise<void>;
+    _addItemsBefore(items: Clutter.Actor, sibling: Clutter.Actor, colSpan?: number): void;
+}
 
 /**
  * @version 46
@@ -15,7 +72,7 @@ export class Panel extends St.Widget {
     statusArea: {
         appMenu: any;
         dateMenu: DateMenuButton;
-        quickSettings: any;
+        quickSettings: QuickSettings;
     };
     menuManager: PopupMenuManager;
 
